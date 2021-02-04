72edfad089a32e5bd321b6ee0411b9886f2fcc87
Susie Zhou (CSI Interfusion Inc) <v-yuwzh@microsoft.com>
2021-02-03 23:24:55 -0800

    gg

---
+++ SimpleWebAppWithIndAuth1/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SimpleWebAppWithIndAuth1.Data;
+using SimpleWebAppWithIndAuth1.Models;
+
+namespace SimpleWebAppWithIndAuth1.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SimpleWebAppWithIndAuth1.Data.ApplicationDbContext _context;
+
+        public CreateModel(SimpleWebAppWithIndAuth1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
